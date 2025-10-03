28979aa50602705aff3741a11c5a1c8bb7f88e2b
karthik nadar <karthiknadar1204@gmail.com>
2025-10-03 14:38:45 +0530

    build screw ups

--- app/api/stripe/webhook/route.ts
+++ app/api/stripe/webhook/route.ts
@@ -2,7 +2,7 @@ import { prisma } from '@/app/lib/db'
 import Stripe from 'stripe'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-    apiVersion: '2025-05-28.basil'
+    apiVersion: '2025-08-27.basil'
 })
 
 export async function POST(req: Request) {
